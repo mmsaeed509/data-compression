6d2c15bda5da711350b53369bd68784b5327ce7a
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2020-11-21 16:38:39 +0200

    upload
    upload project

---
+++ data_compression/src/LZ77/Main.java
@@ -0,0 +1,9 @@
+package LZ77;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ data_compression/src/LZ78/Main.java
@@ -0,0 +1,9 @@
+package LZ78;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
